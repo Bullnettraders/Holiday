e8334313c6ff13833f0deb25f7b04fd7324ae806
Bullnettraders <witz.m@hotmail.com>
2025-05-24 09:08:44 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 import discord
 import os
-import asyncio
-from datetime import datetime, time
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from discord.ext import tasks
 from holiday_fetcher import get_upcoming_holidays
+from zoneinfo import ZoneInfo  # Nur ab Python 3.9+
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -13,21 +13,23 @@ CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
-@tasks.loop(time=time(hour=8, minute=0))  # Jeden Tag um 8:00 Uhr UTC
+@tasks.loop(minutes=1)
 async def daily_feiertags_check():
     await client.wait_until_ready()
-    channel = client.get_channel(CHANNEL_ID)
-    holidays = get_upcoming_holidays(["DE", "US"], days_ahead=2)
-    if holidays:
-        msg_lines = ["**Feiertags-Erinnerung in 2 Tagen:**"]
-        for code, local, eng, date_str in holidays:
-            flag = ":flag_de:" if code == "DE" else ":flag_us:"
-            msg_lines.append(f"{flag} **{local}** ({eng}) – am {date_str}")
-        msg = "\n".join(msg_lines)
-    else:
-        msg = "In 2 Tagen ist kein Feiertag in Deutschland oder den USA."
+    now_berlin = datetime.now(ZoneInfo("Europe/Berlin"))
+    if now_berlin.hour == 8 and now_berlin.minute == 0:
+        channel = client.get_channel(CHANNEL_ID)
+        holidays = get_upcoming_holidays(["DE", "US"], days_ahead=2)
+        if holidays:
+            msg_lines = ["**Feiertags-Erinnerung in 2 Tagen:**"]
+            for code, local, eng, date_str in holidays:
+                flag = ":flag_de:" if code == "DE" else ":flag_us:"
+                msg_lines.append(f"{flag} **{local}** ({eng}) – am {date_str}")
+            msg = "\n".join(msg_lines)
+        else:
+            msg = "In 2 Tagen ist kein Feiertag in Deutschland oder den USA."
 
-    await channel.send(msg)
+        await channel.send(msg)
 
 @client.event
 async def on_ready():
