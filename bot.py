4aea41a05a7a96dbcf1c0e6cb49e3e84a6ec1002
Bullnettraders <witz.m@hotmail.com>
2025-05-24 09:06:34 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import discord
 import os
 import asyncio
-from datetime import datetime, time, timedelta
+from datetime import datetime, time
 from dotenv import load_dotenv
+from discord.ext import tasks
 from holiday_fetcher import get_upcoming_holidays
 
 load_dotenv()
@@ -12,33 +13,27 @@ CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
+@tasks.loop(time=time(hour=8, minute=0))  # Jeden Tag um 8:00 Uhr UTC
 async def daily_feiertags_check():
     await client.wait_until_ready()
     channel = client.get_channel(CHANNEL_ID)
-    
-    while not client.is_closed():
-        now = datetime.now()
-        target_time = datetime.combine(now.date(), time(hour=8))  # 8 Uhr morgens
-        if now > target_time:
-            target_time += timedelta(days=1)
-        wait_time = (target_time - now).total_seconds()
-        await asyncio.sleep(wait_time)
+    holidays = get_upcoming_holidays(["DE", "US"], days_ahead=2)
+    if holidays:
+        msg_lines = ["**Feiertags-Erinnerung in 2 Tagen:**"]
+        for code, local, eng, date_str in holidays:
+            flag = ":flag_de:" if code == "DE" else ":flag_us:"
+            msg_lines.append(f"{flag} **{local}** ({eng}) – am {date_str}")
+        msg = "\n".join(msg_lines)
+    else:
+        msg = "In 2 Tagen ist kein Feiertag in Deutschland oder den USA."
 
-        holidays = get_upcoming_holidays(["DE", "US"], days_ahead=2)
-        if holidays:
-            msg_lines = ["**Feiertags-Erinnerung in 2 Tagen:**"]
-            for code, local, eng, date_str in holidays:
-                flag = ":flag_de:" if code == "DE" else ":flag_us:"
-                msg_lines.append(f"{flag} **{local}** ({eng}) – am {date_str}")
-            msg = "\n".join(msg_lines)
-        else:
-            msg = "In 2 Tagen ist kein Feiertag in Deutschland oder den USA."
-
-        await channel.send(msg)
+    await channel.send(msg)
 
 @client.event
 async def on_ready():
     print(f'{client.user} ist online!')
+    if not daily_feiertags_check.is_running():
+        daily_feiertags_check.start()
 
 @client.event
 async def on_message(message):
@@ -56,5 +51,4 @@ async def on_message(message):
             msg = "In 2 Tagen ist kein Feiertag in Deutschland oder den USA."
         await message.channel.send(msg)
 
-client.loop.create_task(daily_feiertags_check())
 client.run(TOKEN)
